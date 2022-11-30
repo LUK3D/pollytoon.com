33395fc15071da794ef93966645b2c8213effae0
Filipe Lukebana <filipelukebana@gmail.com>
2022-11-29 22:13:38 -0300

    🚀 Added troisjs lib for 3D rendering

---
+++ src/components/PollyViewer.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+// defineProps<{ msg: string }>()
+
+</script>
+
+<template>
+
+  <div class="w-screen h-screen overflow-hidden ">
+
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
