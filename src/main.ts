1496141bfae883bf6cb111e9e1d786dd92c1c9e7
Filipe Lukebana <filipelukebana@gmail.com>
2022-11-29 23:50:39 -0300

    📉 Added vercel Analytics

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from 'vue'
 import './style.css'
 import 'virtual:windi.css'
 import App from './App.vue'
-
+import { inject } from '@vercel/analytics';
 import { TroisJSVuePlugin } from 'troisjs';
 
 const app = createApp(App);
@@ -10,3 +10,5 @@ app.use(TroisJSVuePlugin);
 
 
 app.mount('#app')
+//injecting Vercel Analytics
+inject();
