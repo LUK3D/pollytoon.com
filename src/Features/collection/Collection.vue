d5c9d80d85750d620777decaf3b068fda32debe8
Filipe Lukebana <filipelukebana@gmail.com>
2022-12-08 11:44:04 -0300

    🔥Added Mood Collection

--- src/Features/collection/Collection.vue
+++ src/Features/collection/Collection.vue
@@ -12,8 +12,7 @@ const props = defineProps({
 <template >
     <div
         class="w-full p-2 md:p-5 mt-20  grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 grid-rows-2 gap-2 md:gap-4 lg:gap-8">
-        <div v-for="(item, index) in [1, 2, 3, 4]" :key="index"
-            class="col-span-1  bg-ll-100 rounded-md flex flex-col rounded-xl overflow-hidden relative border-2">
+        <div class="col-span-1  bg-ll-100 rounded-md flex flex-col rounded-xl overflow-hidden relative border-2">
             <div class="w-full h-80 grid grid-cols-2 grid-rows-2 gap-1 ">
                 <img class=" col-span-1 rows h-full w-full object-cover    "
                     src="../../assets/images/collection01/naruto_02.png" alt="">
@@ -55,7 +54,55 @@ const props = defineProps({
                 for
                 free
             </p>
-            <a href="https://zona.luk3d.com/cdn/pollytoon/pollytoon.com.collection.naruto.zip" target="_blank" download
+            <a href="https://cdn.pollytoon.luk3d.com/pollytoon.com.collection.naruto.zip" target="_blank" download
+                v-show="props.user?.email"
+                class="w-full  bg-ll-primary text-white py-2 text-center flex items-center justify-center">
+                <p>Download</p>
+            </a>
+        </div>
+        <div class="col-span-1  bg-ll-100 rounded-md flex flex-col rounded-xl overflow-hidden relative border-2">
+            <div class="w-full h-80 grid grid-cols-2 grid-rows-2 gap-1 ">
+                <img class=" col-span-1 rows h-full w-full object-cover    "
+                    src="../../assets/images/collection02/01.png" alt="">
+                <img class=" col-span-1 rows h-full w-full object-cover    "
+                    src="../../assets/images/collection02/02.png" alt="">
+                <img class=" col-span-1 rows h-full w-full object-cover    "
+                    src="../../assets/images/collection02/03.png" alt="">
+                <img class=" col-span-1 rows h-full w-full object-cover    "
+                    src="../../assets/images/collection02/04.png" alt="">
+            </div>
+
+            <p class="absolute top-2 right-2 bg-ll-primary text-white px-2 rounded-md ">3D included</p>
+
+            <div class="w-full p-2 text-sm flex flex-col bg-ll-200">
+                <ul class="flex flex-col text-gray-400 w-full">
+                    <li class="mb-2 flex">
+                        <p>Collection:</p>
+                        <p class="ml-3">Mood</p>
+                    </li>
+                    <li class="mb-2 flex">
+                        <p>Original Size:</p>
+                        <p class="ml-3">2084 x 2084</p>
+                    </li>
+                    <li class="mb-2 flex">
+                        <p>File Types:</p>
+                        <p class="ml-3">png, .blend</p>
+                    </li>
+                    <li class="mb-2 flex">
+                        <p>File Size:</p>
+                        <p class="ml-3">18.4MB</p>
+                    </li>
+                    <li class="mb-2 flex text-xs border-t pt-2">
+                        <p>5 different expressions and 1 3D file</p>
+
+                    </li>
+                </ul>
+            </div>
+            <p v-show="!props.user?.email" class="w-full text-ll-primary text-center py-3  bg-ll-200">Login To download
+                for
+                free
+            </p>
+            <a href="https://cdn.pollytoon.luk3d.com/pollytoon.com.collection.mood.zip" target="_blank" download
                 v-show="props.user?.email"
                 class="w-full  bg-ll-primary text-white py-2 text-center flex items-center justify-center">
                 <p>Download</p>
